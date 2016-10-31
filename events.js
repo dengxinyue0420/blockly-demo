3756cdc19498217e9256fc1f7a0ce51a65a0381f
Xinyue Deng <dxy0420@mit.edu>
2016-10-31 02:58:23 -0400

    server update

--- events.js
+++ events.js
@@ -2,20 +2,28 @@ var redis = require('redis')
 var sub = redis.createClient();
 var pub = redis.createClient();
 
-sub.subscribe('newBlock');
+//sub.subscribe('newBlock');
 
+var channel = "";
 var events = function(io){
     io.on('connection', function(socket){
-	       socket.on('block', function(msg){
-               console.log("receive new block event for publishing");
-               console.log(msg);
-               pub.publish('newBlock', msg);
-           });
+        console.log("connection is on");
+        socket.on('channel', function(msg){
+            console.log("subscribe to channel "+msg);
+            channel = msg;
+            sub.subscribe(channel);
+        });
 
-           sub.on('message', function(channel, msg){
-               console.log("ready to subscribe "+channel+" "+msg);
-               socket.emit(channel, msg);
-           });
+       socket.on('block', function(msg){
+           console.log("receive new block event for publishing");
+           console.log(msg);
+           pub.publish(channel, msg);
+       });
+
+       sub.on('message', function(ch, msg){
+           console.log("ready to emit "+channel+" "+msg);
+           socket.emit(ch, msg);
+       });
     });
 }
 
