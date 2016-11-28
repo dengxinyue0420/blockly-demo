b95e5dca9e5a2b4e312df57a87be01df8e6f2aed
Xinyue Deng <dxy0420@mit.edu>
2016-11-28 03:29:03 -0500

    server handle project explorer changes

--- events.js
+++ events.js
@@ -4,32 +4,37 @@ var pub = redis.createClient();
 
 sub.subscribe('newBlock');
 
-var channel = "";
 var events = function(io){
     io.on('connection', function(socket){
         console.log("connection is on");
+        // Subscribe to channel
         socket.on('channel', function(msg){
-            console.log("subscribe to channel "+msg);
-            channel = msg;
-            sub.subscribe(channel);
+            console.log("subscribe to user channel "+msg);
+            sub.subscribe(msg);
+        });
+        // Publish changes to user channel when a project is shared
+        socket.on('shareProject', function(msg){
+            console.log("receive new project published to "+msg["channel"]);
+            console.log(msg);
+            pub.publish(msg["channel"], JSON.stringify(msg));
+        });
+        // Publish changes to screen channel when blocks changed
+        socket.on('block', function(msg){
+            console.log("receive new block event for publishing");
+            console.log(msg);
+            pub.publish(msg["channel"], JSON.stringify(msg));
+        });
+        // Test
+        socket.on('testblock', function(msg){
+            console.log("receive new block event for publishing");
+            console.log(msg);
+            pub.publish("newBlock", msg);
         });
 
-       socket.on('block', function(msg){
-           console.log("receive new block event for publishing");
-           console.log(msg);
-           pub.publish(channel, msg);
-       });
-
-       socket.on('testblock', function(msg){
-           console.log("receive new block event for publishing");
-           console.log(msg);
-           pub.publish("newBlock", msg);
-       });
-
-       sub.on('message', function(ch, msg){
-           console.log("ready to emit "+channel+" "+msg);
-           socket.emit(ch, msg);
-       });
+        sub.on('message', function(ch, msg){
+            console.log("ready to emit "+ch+" "+msg);
+            socket.emit(ch, msg);
+        });
     });
 }
 
